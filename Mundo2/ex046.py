4226d1d755797a086614c36b7d91b38432af483d
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2025-12-28 22:54:26 -0300

    contagem de fogos

--- Mundo2/ex046.py
+++ Mundo2/ex046.py
@@ -1,4 +1,5 @@
-n = int(input('Digite um numero: '))
-for c in range(n):
-    print(c)
-#testando git
+for i in range(1, 10, -1):
+    n = int(input('A CONTAGEM COMEÇA EM: {}'.format(i)))
+    print(n)
+print(n)
+# faça uma contagem regressiva para estouros de fogos de artif. indo do 10 ate o 0 com uma pausa de 1seg
