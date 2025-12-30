849a25e12e809645e3d8bd620a6530cecd56693d
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2025-12-30 13:18:25 -0300

    correção: soma dos numeros pares de 1 a 6

--- Mundo2/ex050.py
+++ Mundo2/ex050.py
@@ -1,6 +1,9 @@
-s = 0
-for i in range(1, 6):
-    if i%3!=0:
-        s+=i
-        print(f'A soma é: {s}')
+s=0
+c=0
+for i in range(1,7):
+    n = int(input(f'Digite o {i}° numero: '))
+    if n%2==0:
+        s+=n
+        c+=1
+print(f'A soma de {c} numeros pares, é igual à: {s}')
 # leia 6 numeros int e mostre a soma somente de num pares, se o valor for impar, desconsidere
