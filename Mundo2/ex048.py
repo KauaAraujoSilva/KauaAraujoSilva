a89a3482f2338123b8cbdc3212f6ed53b95745c4
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2025-12-30 12:55:02 -0300

    alterações

--- Mundo2/ex048.py
+++ Mundo2/ex048.py
@@ -1,6 +1,8 @@
 s = 0
-for i in range(1,500):
-    if i%3==0 and i%2!=0:
+c = 0
+for i in range(1,501, 2):
+    if i%3==0:
+        c += 1
         s += i
-print(s)
+print(f'A soma dos {c} valores é de: {s}')
 # soma de 1 ate 500 de num multiplo de 3 e impares
