6ee8b2024008fe51b6c7c0c6dd1e74e8bf9fa37c
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2025-12-30 22:40:43 -0300

    aprendendo ainda a mexer com a condição For

--- Mundo2/ex053.py
+++ Mundo2/ex053.py
@@ -1,3 +1,12 @@
-nome = str(input('Digite a frase: ')).split()
-print(nome)
+frase = str(input('Digite a frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto)-1, -1, -1):
+    inverso += junto[letra]
+# print(inverso, junto)
+if inverso == junto:
+    print('Essa frase é um polidromo')
+else:
+    print('Não é um polidromo')
 # leia frase qualquer e diga se é um palidromo(desconsidere espaços )
