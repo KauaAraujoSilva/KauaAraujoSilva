17f87d5926279106ab487a4e367d4c88dcaee796
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2025-12-30 22:09:00 -0300

    refiz o exercicio

--- Mundo2/ex051.py
+++ Mundo2/ex051.py
@@ -1,11 +1,8 @@
-pritermo = int(input('Digite o valor do primeiro termo: '))
-razão = int(input('Digite o valor da razão da P.A.: '))
-decimo = pritermo + (10 - 1)*razão
-# for i in range(pritermo, decimo+razão, razão):
-#         print(f'{i}', end='-')
-# print('FIM')
-for n in range(1,11):
-    print(f'\033[32m{n}° termo: {pritermo + (n-1)*razão}\033[m')
-
+primeiro = int(input('Digite o primeiro valor: '))
+razão = int(input('Digite o valor da razão: '))
+decimo = primeiro + (10-1)*razão
+for i in range(primeiro, decimo+1, razão):
+    print(i, end=' - ')
+print('Fim')
 # an=a1 +(n-1)*r
 # leia 1° termo e a razãode uma P.A.. mostre os 10 primeiros termos
