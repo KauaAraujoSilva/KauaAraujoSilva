46f4228d085600d55a55e5b2e8435d14048664d0
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2025-12-30 22:54:53 -0300

    correção de bugs no sistema de nascimento

--- Mundo2/ex054.py
+++ Mundo2/ex054.py
@@ -1,5 +1,14 @@
-from time import localtime
-for i in range(1, 8):
-     n = int(input('Digite o seu ano de nascimento: '))
-print(max(n))
+from datetime import date
+atual = date.today().year
+maioridade = 0
+menoridade = 0
+for i in range (1, 8):
+        nasc = int(input('Em que ano a pessoa nasceu?: '))
+        idade = atual - nasc
+        if idade>=21:
+                maioridade+=1
+        else:
+                menoridade+=1
+print(f'Tivemos {maioridade} pessoas maior de idade')
+print(f'Tivemos tambem, {menoridade} pessoas menor de idade')
 # leia ano de nasc. de 7 pessoas, e mostre se atingiu a maioridade(21anos)
