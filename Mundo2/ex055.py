1b09aa3f3ce4139b015c7209e4c79fdb37ffe258
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2026-01-01 17:27:09 -0300

    Mostrar peso

--- Mundo2/ex055.py
+++ Mundo2/ex055.py
@@ -1,5 +1,7 @@
-for i in range(1,6):
-    peso = float(input('Insira o seu peso atual: '))
-    print('O maior peso é: {}'.format((max(peso))))
-    print('O menor peso é: {}'.format((min(peso))))
-# leia o peso de 5 pessoas e mostre o maior e menor
+peso = []
+for i in range(5):
+    valor = float(input('Digite o seu peso: '))
+    peso.append(valor)
+print(f'O maior peso é {max(peso)}')
+print(f'O menor peso é {min(peso)}')
+#leia o peso de 5 pessoas e mostre o maior e menor
