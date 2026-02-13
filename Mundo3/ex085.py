dcd0e409e299b827948963035ed5b4dffc29acc1
Kauã <139062285+KauaAraujoSilva@users.noreply.github.com>
2026-02-12 21:15:32 -0300

    fazendo exericios de python pt2

--- Mundo3/ex085.py
+++ Mundo3/ex085.py
@@ -1,12 +1,10 @@
-lista = []
+lista = [[],[]]
 for n in range(7):
     Value = int(input(f'Digite o {n}o. valor: '))
-    par = Value%2==0
-    impar = Value%2!=0
-    if par:
-        lista.append(Value)
-        print(par)
-    if impar:
-        lista.append(Value)
-print(par)
-print(impar)
+    if Value%2==0:
+        lista[0].append(Value)
+    if Value%2!=0   :
+        lista[1].append(Value)
+Lista_ordenada = sorted(lista)
+print(f'Os valores pares sao: {Lista_ordenada[0]}')    
+print(f'Os valores impares sao: {Lista_ordenada[1]}')    
